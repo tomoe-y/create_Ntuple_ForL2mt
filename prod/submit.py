6f8c42bcd9ce841c7e04e95397157d18b7b66f0f
Tomoe Yamashita <toyamash@login01.icepp.jp>
2023-08-04 17:22:50 +0900

    add grid job

---
+++ prod/submit.py
@@ -0,0 +1,31 @@
+import os
+
+user    = 'user.toyamash' # 'group.det-muon'
+athena  = '22.0.105'
+version = 'Run3-' + athena + '-00-01'
+retry   = '1'
+
+listfile = open('RUN3_l2mt.list')
+AODs = listfile.readlines() 
+listfile.close()
+
+for AOD in AODs:
+    AOD = AOD.rstrip()
+    command =  'pathena MyAnalysis/ATestRun_jobOptions.py'
+    #command += ' %IN %OUT.root"'
+    #command += ' --athenaTag=%s' % athena
+    #command += ' --nGBPerJob=MAX'
+    #command += ' --nFilesPerJob=1'
+    #command += ' --forceStaged'
+    command += ' --inDS=%s' % AOD
+    
+    #output = ESD.replace('DESDM_MCP', 'NTUP_MCP.' + retry)
+    #output = output.replace('DESDM_ZMUMU', 'NTUP_ZMUMU.' + retry)
+    #output = output.replace('DESDM_TILEMU', 'NTUP_TILEMU.' + retry)
+    #output = output.replace('ESD', 'NTUP.' + retry)
+    output = AOD.replace('recon.AOD', 'NTP' + retry)
+    
+    command += ' --outDS=%s.%s.%s_NTUP' % (user, output, version)
+
+    print(command)
+    os.system(command)
